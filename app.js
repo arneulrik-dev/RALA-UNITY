178d905ac6f9b31f6055b80aa4f88484ce47253d
arneulrik-dev <arneulrik@gmail.com>
2026-09-24 16:18:48 +0200

    Koble RALA GO til RALA NORDIC

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const config={goUrl:"",safeUrl:"",safeSubscriptionActive:true};
+const config={goUrl:"https://rala-opzfqbw4x-rala-go.vercel.app",safeUrl:"",safeSubscriptionActive:true};
 const safeCard=document.querySelector("#safe-card");
 const safeButton=document.querySelector("#safe-button");
 if(!config.safeSubscriptionActive){safeCard.classList.add("locked");safeButton.textContent="LÅST";safeButton.disabled=true;}
